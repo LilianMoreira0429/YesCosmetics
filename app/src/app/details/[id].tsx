831944e5e1688429b298c44b331a2966a3612dee
LilianMoreira0429 <lilianpm22@yahoo.com.br>
2024-11-18 16:52:48 -0300

    Ajuste fonte lista

--- app/src/app/details/[id].tsx
+++ app/src/app/details/[id].tsx
@@ -6,8 +6,8 @@ import { useProductDatabase } from "@/database/useProductDatabase";
 
 export default function Details() {
   const [data, setData] = useState({
-    produto: "",
-    quantidade: 0,
+    name: "",
+    quantity: 0,
   });
 
   const productDatabase = useProductDatabase();
@@ -18,8 +18,8 @@ export default function Details() {
       productDatabase.show(Number(params.id)).then((response) => {
         if (response) {
           setData({
-            produto: response.produto,
-            quantidade: response.quantidade,
+            name: response.name,
+            quantity: response.quantity,
           });
         }
       });
@@ -29,11 +29,11 @@ export default function Details() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 16 }}>
       {/* Ajustando a posição dos textos */}
-      <Text style={{ fontSize: 40, marginTop: 20 }}>ID: {params.id} </Text>
+      <Text style={{ fontSize: 30, marginTop: 20 }}>ID: {params.id} </Text>
 
-      <Text style={{ fontSize: 40, marginTop: 10 }}>Quantidade: {data.quantidade}</Text>
+      <Text style={{ fontSize: 30, marginTop: 10 }}>Quantidade: {data.quantity}</Text>
 
-      <Text style={{ fontSize: 40, marginTop: 10 }}>Produto: {data.produto}</Text>
+      <Text style={{ fontSize: 30, marginTop: 10 }}>Produto: {data.name}</Text>
     </View>
   );
 }
